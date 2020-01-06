0a52aa43e63ad3005e6c6d86b8cbea3e344bfb00
Afonso <afonsom98@gmail.com>
2020-01-06 17:10:46 +0000

    User and Review Tables for the DB

--- db/create.js
+++ db/create.js
@@ -1,11 +1,30 @@
 const connection = require('./config');
 
+connection.query(`CREATE TABLE review (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(250) NOT NULL,
+    email VARCHAR(250) NOT NULL,
+    commentPT TEXT(2000),
+    titlePT VARCHAR(250),
+    company VARCHAR(250),
+    isApproved BOOLEAN DEFAULT 0,
+    isShowing BOOLEAN DEFAULT 0,
+    commentEN TEXT(2000),
+    titleEN VARCHAR(250)
+)`, (err) => {
+    if(err) console.log(err);
+    console.log('query complete');
+    // connection.end();
+})
+
 connection.query(`CREATE TABLE user (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(320) NOT NULL UNIQUE,
-    passwordHash VARBINARY(160) NOT NULL
+    name VARCHAR(250) NOT NULL ,
+    passwordHash VARBINARY(160) NOT NULL,
+    IS_SA BOOLEAN NOT NULL
 )`, (err) => {
     if(err) console.log(err);
     console.log('query complete');
     connection.end();
 })
+
