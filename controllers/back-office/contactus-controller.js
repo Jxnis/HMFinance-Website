613fdc8faeefe3fda55e6f34b3131872f9eaa50f
tilda21 <tilda_26@hotmail.com>
2020-01-08 18:19:57 +0000

    Before running the app run yarn db:seed to fill the contact us table

---
+++ controllers/back-office/contactus-controller.js
@@ -0,0 +1,20 @@
+const Contactus = require('../../models/back-office/Contactus');
+
+const getContactus = (req, res) => {
+	
+	Contactus.get((err, results) => {
+		if (err) {
+			//  If an error has occurred, then the user is informed of the error
+			res.status(500).json({ message: 'Error getting all the contact information' });
+		} else {
+			//console.log('We got the contacts succefully');
+			//console.log(results[0].address1);
+			let contactInfo = results[0];
+			res.render('back-office/contactus', { contactInfo });
+		}
+	});
+};
+
+module.exports = {
+	getContactus
+};
--- models/back-office/Review.js
+++ models/back-office/Review.js
@@ -3,15 +3,9 @@ const connection = require('../../db/config');
 const Review = {};
 
 Review.add = (reviewInfo, callback) => {
-<<<<<<< HEAD
-	console.log('Hi');
-	connection.query(
-		`INSERT INTO review (name)
-=======
 	console.log(reviewInfo);
 	connection.query(
 		`INSERT INTO review (name, email, commentPT, titlePT, company, commentEN, titleEN, isApproved, isShowing)
->>>>>>> cdb720ec5c770998eb2c45f6bb3ac63b0c0740fe
             VALUES (
                 ?, ?, ?, ?, ?, ?, ?, ?, ?
             )
