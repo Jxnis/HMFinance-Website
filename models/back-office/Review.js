98f5f370f76b39c84e4651cd18c12e20403985b7
Afonso <afonsom98@gmail.com>
2020-01-08 15:36:57 +0000

    Add a review to DB form

--- models/back-office/Review.js
+++ models/back-office/Review.js
@@ -3,14 +3,14 @@ const connection = require('../../db/config');
 const Review = {};
 
 Review.add = (reviewInfo, callback) => {
-    console.log('Hi')
-    connection.query(
-		`INSERT INTO review (name)
+	console.log(reviewInfo);
+	connection.query(
+		`INSERT INTO review (name, email, commentPT, titlePT, company, commentEN, titleEN, isApproved, isShowing)
             VALUES (
-                'Afonso'
+                ?, ?, ?, ?, ?, ?, ?, ?, ?
             )
         `,
-		// [reviewInfo.email, reviewInfo.password],
+		[reviewInfo.name, reviewInfo.email, reviewInfo.commentPT, reviewInfo.titlePT, reviewInfo.company, reviewInfo.commentEN, reviewInfo.titleEN, reviewInfo.isApproved, reviewInfo.isShowing],
 		(err, results, fields) => callback(err, results, fields)
 	);
 };
