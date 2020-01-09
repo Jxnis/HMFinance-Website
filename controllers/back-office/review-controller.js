c43b7f8b08623b258b52ade5b207ba6a3cfe821c
Afonso <afonsom98@gmail.com>
2020-01-09 17:35:16 +0000

    Review Back Office Finished

--- controllers/back-office/review-controller.js
+++ controllers/back-office/review-controller.js
@@ -3,7 +3,7 @@ const Review = require('../../models/back-office/Review');
 const addReview = (req, res) => {
 	Review.add(req.body, (err) => {
 		if (err) return res.render('error', { err });
-		res.redirect('/dashboard/review');
+		res.redirect('/dashboard/reviews');
 	});
 };
 
@@ -13,7 +13,6 @@ const getReviews = (req, res) => {
 			//  If an error has occurred, then the user is informed of the error
 			res.status(500).json({ message: 'Error getting all the contact information' });
 		} else {
-			console.log(results);
 			let reviews = results;
 			res.render('back-office/review', {user: req.user, reviews});
 		}		
@@ -23,13 +22,39 @@ const getReviews = (req, res) => {
 const editReview = (req, res) => {
 	Review.edit(req.body, (err) => {
 		if (err) return res.render('error', { err });
-		res.redirect('/dashboard/review');
+		res.redirect('/dashboard/reviews');
+	});
+};
+
+// Renders the Page with the Form to add a new review
+const getAddReviewPage = (req, res) => {
+	res.render('back-office/addReview');
+};
+
+// Renders the Page with the Form to edit the review
+const getEditReviewPage = (req, res) => {
+	Review.getById(req.params.id, (err, results) => {
+		if(err) return res.render('error', { err });
+		let review = results[0];
+		res.render('back-office/editReview', { review });
+	});
+};
+
+// Deletes a review from the DB
+const deleteReview = (req, res) => {
+	console.log(req.params.id);
+	Review.delete(req.params.id, (err) => {
+		if(err) return res.render('error', { err });
+		res.redirect('/dashboard/reviews');
 	});
 };
 
 module.exports = {
 	addReview,
 	getReviews,
-	editReview
+	editReview,
+	getAddReviewPage,
+	getEditReviewPage,
+	deleteReview
 };
 
--- routes/back-office/dashboard.js
+++ routes/back-office/dashboard.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { authorize } = require('../../controllers/back-office/auth-controller');
-const { addReview, getReviews, editReview } = require('../../controllers/back-office/review-controller');
+const { addReview, getReviews, editReview, getAddReviewPage, getEditReviewPage, deleteReview } = require('../../controllers/back-office/review-controller');
 const { getContactus } = require('../../controllers/back-office/contactus-controller');
 
 
@@ -11,13 +11,23 @@ router.get('/', authorize, (req, res) => {
 
 //Review
 // Render review page backoffice
-router.get('/review', authorize, getReviews);
+router.get('/reviews', authorize, getReviews);
+
+// Render add review page backoffice
+router.get('/reviews/new', authorize, getAddReviewPage);
+
+// Render edit review page backoffice
+router.get('/reviews/:id/edit', authorize, getEditReviewPage);
 
 // Add a review
-router.post('/review', authorize, addReview);
+router.post('/reviews', authorize, addReview);
 
 // Edit a review
-router.post('/review/edit', authorize, editReview);
+router.post('/reviews/edit', authorize, editReview);
+
+// Delete a review
+router.post('/reviews/:id/delete', authorize, deleteReview);
+
 
 //ContactUs
 router.get('/contactus', authorize, getContactus);
