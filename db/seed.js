c4be8b2d0fe747de9540ee618f648d16741b1423
tilda21 <tilda_26@hotmail.com>
2020-01-12 15:41:34 +0000

    Backoffice homepage done

--- db/seed.js
+++ db/seed.js
@@ -1,5 +1,5 @@
 const connection = require('./config');
-
+/* 
 connection.query(`INSERT INTO user (email, passwordHash, IS_SA) VALUES (
     'hugotavaresmaia@gmail.com',
 	SHA2('HMFinance!?#', 256),
@@ -29,4 +29,29 @@ connection.query(
 		if(err) console.log(err);
 		console.log('query complete');
 		connection.end();
+});
+ */
+connection.query(`INSERT INTO homepage (titlePT, titleEN, introPT, introEN, aboutUsPT, aboutUsEN) VALUES (
+	'HMFinance',
+	'HMFinance',
+	'Serviços de Contablidade',
+	'Accounting Services',
+	'O nosso projeto teve início em 2014, com a constituição de uma equipa formada em Contabilidade, Fiscalidade, Auditoria e Gestão, pelo Instituto Superior de Contabilidade e Administração de Lisboa (ISCAL).
+	O nosso quadro de pessoal caracteriza-se por ser jovem, capacitado, dinâmico e empenhado.
+	Com experiência em PMEs e Multinacionais, a HM Finance está em fase de expansão, com a abertura de um novo escritório no Distrito de Setúbal.
+	A qualidade dos nosso serviços e a sua satisfação motivam a nossa melhoria contínua. Atualmente, iniciamos o processo de Certificação do Sistema de Gestão de Qualidade, de acordo com a norma ISO 9001.
+	Assumimos um compromisso com o seu negócio.
+	Por si e para si.
+	',
+	'Our project began in 2014 with the establishment of a team formed in Accounting, Tax, Audit and Management at the School of Accounting and Administration of Lisbon (ISCAL).
+	Our staff is characterized by being young, skilled, dynamic and committed.
+	With experience in SMEs and Multinationals, HM Finance is expanding, with the opening of a new office in the Setúbal District.
+	The quality of our services and your satisfaction motivates our continuous improvement. 
+	We are currently beginning the Quality Management System Certification process in accordance with ISO 9001.
+	We are committed to your business.
+	For you and for you.'
+)`, (err) => {
+	if(err) console.log(err);
+	console.log('query complete');
+	// connection.end();
 });
