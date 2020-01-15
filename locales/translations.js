525121415139419adf888a795512894addd0dfbe
Jxnis <janis.ellerbrock@gmail.com>
2020-01-15 18:10:01 +0000

    adds Object with translation text

---
+++ locales/translations.js
@@ -0,0 +1,81 @@
+const Translations = {
+	en: {
+		
+		SectionsTitles: {
+			Services: 'Services',
+			Simulators: 'Simulators',
+			AboutUs: 'About us',
+			ContactUs: 'Contact us',
+			Clients: 'Clients'
+
+		},
+		Services: {
+			Titles: {
+				HR: 'HR',
+				Accounting: 'Accounting',
+				Analytical: 'Analytical Accounting',
+				Company: 'Company Constitution',
+				Party: 'Third Party Management',
+				Others: 'Other Services'
+                
+			},
+			HomeText: {
+				HR: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
+				Accounting: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
+				Company: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
+				Party: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
+				Others: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.'
+			},
+
+			ServicesText: {
+				HR: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
+				Accounting: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
+				Analytical: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
+				Company: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
+				Party: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
+				Others: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.'
+			},
+
+			SubTitles: {
+				HR: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
+				Accounting: 'Your Business Financial PT',
+				Company: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
+				Party: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
+				Others: 'European citizens Legalization, IRS Statements, independet workers'
+			}
+		},
+		Simulators: {
+			Titles: {
+				SS: 'Social Secuirty',
+				IMT: 'City Tax on Real Estate Transfers',
+
+			},
+			HomeText: {
+				SS: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
+				IMT: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
+			}
+		},
+            
+		AboutUs: {
+			Title: 'Born in Lisbon',
+			SubTitle: 'i fuckn hate finance stuff',
+			Text: 'Our project started in 2016 with the formation of a team formed in Accounting (ISCAL), Taxation, Auditing and Human Resources Management. Our staff, made up of a team of 7, is characterized by being young, skilled, dynamic and committed. With experience in SMEs and Multinationals, HMFinance is currently being consolidated and expanded in the Lisbon and Cascais Districts. The quality of our services and your satisfaction motivate the our continuous improvement.',                    
+		},
+
+		ContactUs: {
+			FormTitle: 'We love you, contact us',
+			FormName: 'Insert name',
+			FormEmail: 'Insert email',
+			FormMessage: 'Insert message',
+			MapTitle: 'We are here for you. Say hi.',
+		},
+
+		Review: {
+			Title: 'We Rock'
+		}        
+
+            
+	}
+};
+
+module.exports = Translations;
