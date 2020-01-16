3a57b77de6284942edb05c8b4839463216ebc217
Afonso <afonsom98@gmail.com>
2020-01-16 15:11:07 +0000

    3 Simulator Logic + David Test Page Pug

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 const {getAllInfo} = require('../controllers/homepage-controller-front');
 const {getSimulatorInfo} = require('../controllers/simulators-controller-front');
+const {renderTestPage} = require('../controllers/test-controller');
 
 /* GET home page. */
 router.get('/', getAllInfo);
 
 /* Render the simulator page */
 router.get('/simulators', getSimulatorInfo);
+
+router.get('/test', renderTestPage);
 
 module.exports = router;
