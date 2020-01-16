31a09072d38ec14a7b1c9bca1543868239409f91
Jxnis <janis.ellerbrock@gmail.com>
2020-01-16 18:24:19 +0000

    adds signup popup to homepage and connects to  mailchimp acc

---
+++ public/javascripts/mailchimp.js
@@ -0,0 +1,6 @@
+
+const mailchimptoggle = () => {
+	window.dojoRequire(['mojo/signup-forms/Loader'], function(L) { L.start({'baseUrl':'mc.us4.list-manage.com','uuid':'09457e9e38fbf1c62dd79d76b','lid':'9679ff8d78','uniqueMethods':true}); });
+};
+
+module.exports = mailchimptoggle;
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,16 @@ const {getSimulatorInfo} = require('../controllers/simulators-controller-front')
 
 /* GET home page. */
 router.get('/', getBrowserLang, getAllInfo);
-router.get('?language', getAllInfo);
+
 
 /* Render the simulator page */
 router.get('/simulators', getSimulatorInfo);
+
+
+//
+router.post('/signupmail', function (req, res) {
+	// save user details to your database.
+	res.send('Signed Up!');
+});
 
 module.exports = router;
