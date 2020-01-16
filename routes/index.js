e61e29b60b6309dbb8b0f489af916eeec0f3a2d5
tilda21 <tilda_26@hotmail.com>
2020-01-16 11:39:34 +0000

    getting language from browser

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {getAllInfo} = require('../controllers/homepage-controller-front');
+const {getBrowserLang, getAllInfo} = require('../controllers/homepage-controller-front');
 const {getSimulatorInfo} = require('../controllers/simulators-controller-front');
 
 /* GET home page. */
-router.get('/', getAllInfo);
+router.get('/', getBrowserLang, getAllInfo);
 
 /* Render the simulator page */
 router.get('/simulators', getSimulatorInfo);
