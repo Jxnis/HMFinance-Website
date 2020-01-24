7ccb45b0b9649d2962a4548ec144fc7e70546af2
David Saiote <46342828+adikeo@users.noreply.github.com>
2020-01-24 06:00:24 +0000

    Some PUG changes. *WARNING* Gives error:
    TypeError: /mnt/d/Onedrive/WCS/Projects/Project_3/Website/views/index.pug:10
        8|     ul
        9|       li
      > 10|         a#homeid(href='#home') #{translations[locale].SectionsTitles.HMFinance}
    
    Cannot read property 'SectionsTitles' of undefined

--- locales/translations.js
+++ locales/translations.js
@@ -24,7 +24,8 @@ const Translations = {
 			AboutUs: 'About us',
 			ContactUs: 'Contact us',
 			Clients: 'Clients',
-			Reviews: 'Reviews'
+			Reviews: 'Reviews',
+			HMFinance: 'HM Finance'
 
 		},
 		Services: {
@@ -50,7 +51,7 @@ const Translations = {
 				Accounting: 'We select a Personal Trainer to help you with the  tasks, processes and organization of your business. We train your business and guide it so that it is in its best economic state to achieve its goals.',
 				Analytical: 'Analytical accounting is a branch of accounting that has a more internal and detailed approach, so it becomes critical in controlling costs, profit margin and decision making in the management of its activities. HMFinance gives you the data you need to make your decisions on time.',
 				Company: 'HMFinance supports you in the development of your line of business, accompanying you from the beginning of your idea through market research to the goal, indicating to you which type of company and tax regime is most advantageous and efficient for you to achieve your goals.',
-				Party: 'Third Party Management is an essential tool in managing your business. HMFinance makes available through PHC software a certified billing program for: - Billing preparation to customers (up to 5 invoices per month); - Validation and submission of SAFT-PT file;',
+				Party: 'Third Party Management is an essential tool in managing your business. HMFinance makes available through PHC software a certified billing program for: - Billing preparation to customers (up to 5 invoices per month); - Validation and submission of SAFT-pt file;',
 				Others: '- Legalization of European and non-European citizens; - IRS Declaration - Model 3; - Annex G - Property gains; - Opening of the company; - Market studies; - Networking Group HMFinance Events;'
 			},
 
@@ -113,7 +114,8 @@ const Translations = {
 			AboutUs: 'Sobre Nós',
 			ContactUs: 'Contacte-nos',
 			Clients: 'Clientes',
-			Reviews: 'Comentários'
+			Reviews: 'Comentários',
+			HMFinance: 'HM Finance'
 
 		},
 		Services: {
@@ -140,7 +142,7 @@ const Translations = {
 				Accounting: 'Destacamos um Personal Trainer para o acompanhar nas tarefas, processos e organização do seu negócio. Treinamos a sua empresa e orientamo-la de modo a que esteja no seu melhor estado económico, para atingir as suas metas.',
 				Analytical: 'A Contabilidade analítica é um ramo da contabilidade que dispõe de uma abordagem mais interno e detalhado, por isso, torna-se fundamental no controle dos custos, margem de lucro e tomada de decisões na gestão das suas atividades. A HMFinance dá-lhe os dados necessários para que possa tomar as suas decisões atempadamente.',
 				Company: 'A HMFinance apoia-o no desenvolvimento da sua linha de negócio, acompanhando-o desde a ideia embrionária, passando pelo estudo de mercado, até à reta da meta, indicando-lhe para isso qual o tipo de empresa e regime fiscal mais vantajoso e eficiente para atingir os seus objetivos.',
-				Party: 'A Gestão de Terceiros é uma ferramenta essencial na gestão da sua empresa. A HMFinance disponibiliza através do software PHC um programa de faturação certificado para a: - Elaboração da faturação a clientes (até 5 faturas por mês); - Validação e submissão do ficheiro SAFT-PT;',
+				Party: 'A Gestão de Terceiros é uma ferramenta essencial na gestão da sua empresa. A HMFinance disponibiliza através do software PHC um programa de faturação certificado para a: - Elaboração da faturação a clientes (até 5 faturas por mês); - Validação e submissão do ficheiro SAFT-pt;',
 				Others: '- Legalização de cidadãos europeus e não europeus; - Declaração de IRS – Modelo 3; - Anexo G - Mais Valias de imóveis; - Abertura da empresa; - Estudos de mercado; - Eventos Networking Group HMFinance;'
 			},
 
