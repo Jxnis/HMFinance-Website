b14fc6f1bd418fffbdca95c604b59d2cc64a93f8
Afonso <afonsom98@gmail.com>
2020-01-29 16:37:02 +0000

    Email Chimp Pop Up

--- public/javascripts/mailchimp.js
+++ public/javascripts/mailchimp.js
@@ -1,6 +1,9 @@
-
 const mailchimptoggle = () => {
 	window.dojoRequire(['mojo/signup-forms/Loader'], function(L) { L.start({'baseUrl':'mc.us4.list-manage.com','uuid':'09457e9e38fbf1c62dd79d76b','lid':'9679ff8d78','uniqueMethods':true}); });
+};
+
+window.onload = () => {
+	setTimeout(mailchimptoggle, 3000);
 };
 
 module.exports = mailchimptoggle;
